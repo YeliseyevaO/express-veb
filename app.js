962499613253498efe9bdd6adae0737975f6cabd
Olga Yelisyeyeva <eliseevaolgaf5@gmail.com>
2022-06-25 20:29:46 +0300

    use handlebars

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const { engine } = require ('express-handlebars');
+
+app.use(express.static('public'));
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', engine({ 
+     defaultLayout: 'main',
+extname: '.hbs'
+}));
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+res.render('home');
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
